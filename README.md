69c1a9c67207519f82e2397c790215e67cf3936e
Jt Miclat <jtmiclat@gmail.com>
2021-09-29 16:20:54 +0800

    Setup security headers

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cloudflare worker for jtmiclat.me
+
+Adds security header to jtmiclat.me
---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+use worker::*;
+mod utils;
+
+fn log_request(req: &Request) {
+    console_log!(
+        "{} - [{}], located at: {:?}, within: {}",
+        Date::now().to_string(),
+        req.path(),
+        req.cf().coordinates().unwrap_or_default(),
+        req.cf().region().unwrap_or("unknown region".into())
+    );
+}
+
+#[event(fetch)]
+pub async fn main(req: Request, env: Env) -> Result<Response> {
+    log_request(&req);
+
+    // Optionally, get more helpful error messages written to the console in the case of a panic.
+    utils::set_panic_hook();
+    let version = env.var("WORKERS_RS_VERSION")?.to_string();
+    let mut response = worker::Fetch::Request(req).send().await?;
+    let mut headers = response.headers().clone();
+    headers.set(
+        "strict-transport-security",
+        "max-age=31536000; includeSubDomains",
+    )?;
+    headers.set("x-frame-options", "SAMEORIGIN")?;
+    headers.set("x-content-type-options", "nosniff")?;
+    headers.set("referrer-policy", "no-referrer")?;
+    headers.set(
+        "permissions-policy",
+        "microphone 'none'; geolocation 'none'",
+    )?;
+    headers.set(
+        "content-security-policy",
+        "default-src 'self' *.google-analytics.com",
+    )?;
+    headers.set("cf-worker-version", &version)?;
+    Ok(Response::from_html(response.text().await?)?.with_headers(headers))
+}
